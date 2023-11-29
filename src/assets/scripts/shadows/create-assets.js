af35b013098faadd279e1f99e6f3077e63db69e0
moxiethunder <daniel.littlethunder@moxiesozo.com>
2023-11-29 11:27:13 -0700

    Setup controls for shadow cameras and camera helpers

--- src/assets/scripts/shadows/create-assets.js
+++ src/assets/scripts/shadows/create-assets.js
@@ -2,15 +2,23 @@ import GUI from 'lil-gui'
 import * as THREE from 'three'
 
 export const createGUI = (config) => {
-  const { material, ambientLight, pointLight, spotLight, directionalLight, ico, torusKnot, plane, pointLightHelper, spotLightHelper, directionalLightHelper } = config
+  const { material, ambientLight, pointLight, spotLight, directionalLight, ico, torusKnot, plane, pointLightHelper, spotLightHelper, directionalLightHelper, dirTarget, spotTarget, dirLightCamera, dirLightCameraHelper, spotLightCamera, spotLightCameraHelper } = config
+
+  const headings = {
+    dirTarget: 'Directional Light Target',
+    spotTarget: 'Spot Light Target',
+  }
   //GUI
   const gui = new GUI({ width: 300 })
   
-  //SPHERE
-  // const sphereFolder = gui.addFolder('Sphere')
-  // sphereFolder.add(sphere.position, 'x').min(-10).max(10).step(0.001).name('position x')
-  // sphereFolder.add(sphere.position, 'y').min(-10).max(10).step(0.001).name('position y')
-  // sphereFolder.add(sphere.position, 'z').min(-10).max(10).step(0.001).name('position z')
+  // ico
+  // const icoFolder = gui.addFolder('Icosahedron').close
+  // icoFolder.add(ico.position, 'x').min(-10).max(10).step(0.001).name('position x')
+  // icoFolder.add(ico.position, 'y').min(-10).max(10).step(0.001).name('position y')
+  // icoFolder.add(ico.position, 'z').min(-10).max(10).step(0.001).name('position z')
+  // icoFolder.add(ico.rotation, 'x').min(-Math.PI * 2).max(Math.PI * 2).step(0.001).name('rotation x')
+  // icoFolder.add(ico.rotation, 'y').min(-Math.PI * 2).max(Math.PI * 2).step(0.001).name('rotation y')
+  // icoFolder.add(ico.rotation, 'z').min(-Math.PI * 2).max(Math.PI * 2).step(0.001).name('rotation z')
   
   //TORUS KNOT
   // const torusKnotFolder = gui.addFolder('Torus Knot')
@@ -18,11 +26,11 @@ export const createGUI = (config) => {
   // torusKnotFolder.add(torusKnot.position, 'y').min(-10).max(10).step(0.001).name('position y')
   // torusKnotFolder.add(torusKnot.position, 'z').min(-10).max(10).step(0.001).name('position z')
   
-  //CAMERA
-  // const cameraFolder = gui.addFolder('Camera')
-  // cameraFolder.add(config.camera.position, 'x').min(-10).max(10).step(0.001).name('camera x')
-  // cameraFolder.add(config.camera.position, 'y').min(-10).max(10).step(0.001).name('camera y')
-  // cameraFolder.add(config.camera.position, 'z').min(-10).max(10).step(0.001).name('camera z')
+  // CAMERA
+  const cameraFolder = gui.addFolder('Camera').close()
+  cameraFolder.add(config.camera.position, 'x').min(-10).max(10).step(0.001).name('camera x')
+  cameraFolder.add(config.camera.position, 'y').min(-10).max(10).step(0.001).name('camera y')
+  cameraFolder.add(config.camera.position, 'z').min(-10).max(10).step(0.001).name('camera z')
   
   //MATERIAL
   const materialFolder = gui.addFolder('Material').close()
@@ -33,6 +41,8 @@ export const createGUI = (config) => {
 
   //AMBIENT LIGHT
   const ambientLightFolder = gui.addFolder('Ambient Light').close()
+  ambientLightFolder.add(ambientLight, 'visible')
+  ambientLightFolder.addColor(ambientLight, 'color').name('ambient color')
   ambientLightFolder.add(ambientLight, 'intensity').min(0).max(10).step(0.001).name('ambient intensity')
 
   //POINT LIGHT
@@ -41,14 +51,15 @@ export const createGUI = (config) => {
   pointLightFolder.add(pointLightHelper, 'visible').name('show helper')
   pointLightFolder.addColor(pointLight, 'color').name('point color')
   pointLightFolder.add(pointLight, 'intensity').min(0).max(30).step(0.001).name('point intensity')
-  pointLightFolder.add(pointLight.position, 'x').min(-10).max(10).step(0.001).name('point x')
-  pointLightFolder.add(pointLight.position, 'y').min(-10).max(10).step(0.001).name('point y')
-  pointLightFolder.add(pointLight.position, 'z').min(-10).max(10).step(0.001).name('point z')
+  pointLightFolder.add(pointLight.position, 'x').min(-10).max(10).step(0.001).name('position x')
+  pointLightFolder.add(pointLight.position, 'y').min(-10).max(10).step(0.001).name('position y')
+  pointLightFolder.add(pointLight.position, 'z').min(-10).max(10).step(0.001).name('position z')
 
   //SPOT LIGHT
   const spotLightFolder = gui.addFolder('Spot Light').close()
   spotLightFolder.add(spotLight, 'visible')
-  spotLightFolder.add(spotLightHelper, 'visible').name('show helper')
+  spotLightFolder.add(spotLightHelper, 'visible').name('show light helper')
+  spotLightFolder.add(spotLightCameraHelper, 'visible').name('show camera helper')
   spotLightFolder.addColor(spotLight, 'color').name('spot color')
   spotLightFolder.add(spotLight, 'intensity').min(0).max(200).step(0.01)
   spotLightFolder.add(spotLight, 'distance').min(0).max(200).step(0.01)
@@ -58,19 +69,46 @@ export const createGUI = (config) => {
   spotLightFolder.add(spotLight.position, 'y').min(-10).max(10).step(0.01).name('position y')
   spotLightFolder.add(spotLight.position, 'z').min(-10).max(10).step(0.01).name('position z')
 
+  const spotTargetFolder = spotLightFolder.addFolder(headings.spotTarget).open()
+  spotTargetFolder.add(spotTarget.position, 'x').min(-20).max(20).step(0.001).name('target x').onChange(() => spotLightHelper.update())
+  spotTargetFolder.add(spotTarget.position, 'y').min(-20).max(20).step(0.001).name('target y').onChange(() => spotLightHelper.update())
+  spotTargetFolder.add(spotTarget.position, 'z').min(-20).max(20).step(0.001).name('target z').onChange(() => spotLightHelper.update())
+
+  const spotCameraFolder = spotLightFolder.addFolder('Spot Light Camera').open()
+  spotCameraFolder.add(spotLightCamera, 'near').min(0).max(20).step(0.001).name('near').onChange(() => {
+    spotLightCamera.updateProjectionMatrix()
+    spotLightCameraHelper.update()
+  })
+  spotCameraFolder.add(spotLightCamera, 'far').min(0).max(20).step(0.001).name('far').onChange(() => {
+    spotLightCamera.updateProjectionMatrix()
+    spotLightCameraHelper.update()
+  })
+
   //DIRECTIONAL LIGHT
   const directionalLightFolder = gui.addFolder('Directional Light').close()
   directionalLightFolder.add(directionalLight, 'visible')
-  directionalLightFolder.add(directionalLightHelper, 'visible').name('show helper')
+  directionalLightFolder.add(directionalLightHelper, 'visible').name('show light helper')
+  directionalLightFolder.add(dirLightCameraHelper, 'visible').name('show camera helper')
   directionalLightFolder.addColor(directionalLight, 'color').name('directional color')
   directionalLightFolder.add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('directional intensity')
   directionalLightFolder.add(directionalLight.position, 'x').min(-10).max(10).step(0.001).name('directional x')
   directionalLightFolder.add(directionalLight.position, 'y').min(-10).max(10).step(0.001).name('directional y')
   directionalLightFolder.add(directionalLight.position, 'z').min(-10).max(10).step(0.001).name('directional z')
-  // directionalLightFolder.add(directionalLight.rotation, 'x').min(-Math.PI).max(Math.PI).step(0.001).name('directional rotation x')
-  // directionalLightFolder.add(directionalLight.rotation, 'y').min(-Math.PI).max(Math.PI).step(0.001).name('directional rotation y')
-  // directionalLightFolder.add(directionalLight.rotation, 'z').min(-Math.PI).max(Math.PI).step(0.001).name('directional rotation z')
+  
+  const dirTargetFolder = directionalLightFolder.addFolder(headings.dirTarget).open()
+  dirTargetFolder.add(dirTarget.position, 'x').min(-20).max(20).step(0.001).name('target x').onChange(() => directionalLightHelper.update())
+  dirTargetFolder.add(dirTarget.position, 'y').min(-20).max(20).step(0.001).name('target y').onChange(() => directionalLightHelper.update())
+  dirTargetFolder.add(dirTarget.position, 'z').min(-20).max(20).step(0.001).name('target z').onChange(() => directionalLightHelper.update())
 
+  const dirCameraFolder = directionalLightFolder.addFolder('Directional Light Camera').open()
+  dirCameraFolder.add(dirLightCamera, 'near').min(0).max(20).step(0.001).name('near').onChange(() => {
+    dirLightCamera.updateProjectionMatrix()
+    dirLightCameraHelper.update()
+  })
+  dirCameraFolder.add(dirLightCamera, 'far').min(0).max(20).step(0.001).name('far').onChange(() => {
+    dirLightCamera.updateProjectionMatrix()
+    dirLightCameraHelper.update()
+  })
 
   return gui
 }
@@ -83,18 +121,26 @@ export const createLights = () => {
   const pointLight = new THREE.PointLight(0xff0000, 8.25, 100)
   pointLight.position.set(-0.95, 0.65, 1.4)
   const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.1)
+  pointLight.castShadow = true
 
   // DIRECTIONAL LIGHT
   const directionalLight = new THREE.DirectionalLight(0x00ff00, 1)
   directionalLight.position.set(-3.75, 1.65, -3.75)
   const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.1, 0xffffff)
+  directionalLight.castShadow = true
 
   // SPOT LIGHT
-  const spotLight = new THREE.SpotLight(0x0000ff, 95, 10, 0.45, 0.25, 1)
-  spotLight.position.set(-1.5, 1.75, 4.45)
+  const spotLight = new THREE.SpotLight(0xffffff, 6.5, 10, 0.45, 0.25, 1)
+  spotLight.position.set(-3.8, 2.15, 0.35)
   const spotLightHelper = new THREE.SpotLightHelper(spotLight, 0.1)
+  spotLight.castShadow = true
 
-  console.log(spotLightHelper)
+  // const lights = [spotLight, directionalLight, pointLight]
+
+  // lights.forEach(light => {
+  //   light.shadow.mapSize.width = 512 * 4
+  //   light.shadow.mapSize.height = 512 * 4
+  // })
 
   return { ambientLight, pointLight, pointLightHelper, directionalLight, directionalLightHelper, spotLight, spotLightHelper }
 }
@@ -110,18 +156,22 @@ export const createGeometries = () => {
   )
   plane.rotation.x = -Math.PI * 0.5
   plane.position.y = -1.5
+  plane.receiveShadow = true
   
   const ico = new THREE.Mesh(
     new THREE.IcosahedronGeometry(0.8, 0),
     material
   )
-  ico.position.set(-1.25, 1, -1)
+  ico.position.set(0, -0.5, 0)
+  ico.castShadow = true
 
   const torusKnot = new THREE.Mesh(
     new THREE.TorusKnotGeometry(0.65, 0.2, 24, 24),
     material
   )
   torusKnot.position.set(1.35, 0.075, 1.5)
+  torusKnot.castShadow = true
+  torusKnot.receiveShadow = true
 
   return { plane, ico, torusKnot, material }
 }
--- src/assets/scripts/shadows/scene.js
+++ src/assets/scripts/shadows/scene.js
@@ -37,8 +37,8 @@ const Scene = () => {
     requestAnimationFrame(animateControls)
     const elapsedTime = clock.getElapsedTime()
 
-    rotateXY(ico, 2, 2, elapsedTime)
-    rotateXY(torusKnot, 2, 2, elapsedTime)
+    rotateXY(ico, -2, 5, elapsedTime)
+    rotateXY(torusKnot, 3, -2, elapsedTime)
 
     controls.update()
     renderer.render(scene, camera)
@@ -51,23 +51,57 @@ const Scene = () => {
   const clock = new THREE.Clock()
 
   //CAMERA CONFIGURATION
-  const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
-  camera.position.x = -4.25
-  camera.position.y = 6
-  camera.position.z = 5.25
+  const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 1000)
+  camera.position.x = -10
+  camera.position.y = 8
+  camera.position.z = 5.5
 
   //GEOMETRY CONFIGURATION
   const { plane, ico, torusKnot, material } = createGeometries()
-  scene.add(plane, ico, torusKnot)
+  scene.add(plane)
+  scene.add(ico)
+  // scene.add(torusKnot)
 
   //LIGHT CONFIGURATION
   const { ambientLight, pointLight, pointLightHelper, directionalLight, directionalLightHelper, spotLight, spotLightHelper } = createLights()
 
-  directionalLight.target = ico
-  spotLight.target = torusKnot
+  const dirTarget = new THREE.Object3D()
+  directionalLight.target = dirTarget
+  scene.add(dirTarget)
+
+  const spotTarget = new THREE.Object3D()
+  spotLight.target = spotTarget
+  spotTarget.position.set(2.35, -4, -0.25)
+  scene.add(spotTarget)
 
   scene.add(ambientLight, pointLight, pointLightHelper, directionalLight, directionalLightHelper, spotLight, spotLightHelper)
 
+  //DIRECTIONAL LIGHT CAMERA AND SHADOW CONFIGURATION
+  const dirLightCamera = directionalLight.shadow.camera
+  const dirLightShadow = directionalLight.shadow
+
+  const dirLightCameraHelper = new THREE.CameraHelper(dirLightCamera)
+  scene.add(dirLightCameraHelper)
+  dirLightCameraHelper.visible = false
+
+  dirLightShadow.mapSize.width = 512 * 4
+  dirLightShadow.mapSize.height = 512 * 4
+  dirLightCamera.near = 1
+  dirLightCamera.far = 10
+
+  const spotLightCamera = spotLight.shadow.camera
+  const spotLightShadow = spotLight.shadow
+
+  const spotLightCameraHelper = new THREE.CameraHelper(spotLightCamera)
+  scene.add(spotLightCameraHelper)
+  spotLightCameraHelper.visible = false
+
+  spotLightShadow.mapSize.width = 512 * 4
+  spotLightShadow.mapSize.height = 512 * 4
+  spotLightCamera.near = 1
+  spotLightCamera.far = 10
+
+  //HIDE HELPERS
   spotLightHelper.visible = false
   directionalLightHelper.visible = false
   pointLightHelper.visible = false
@@ -85,13 +119,20 @@ const Scene = () => {
     torusKnot,
     material,
     camera,
-    plane
+    plane,
+    dirTarget,
+    spotTarget,
+    dirLightCamera,
+    dirLightCameraHelper,
+    spotLightCamera,
+    spotLightCameraHelper,
   })
 
   //RENDERER CONFIGURATION
   const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvas })
   renderer.setSize(window.innerWidth, window.innerHeight)
   setPixelRatio()
+  renderer.shadowMap.enabled = true
 
   //ORBIT CONTROLS CONFIGURATION
   const controls = new OrbitControls(camera, renderer.domElement)
